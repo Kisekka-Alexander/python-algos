63eec982469a63fd8ea677b831ae6d3f6120ff9d
Kisekka-Alexander <kkalexander717@gmail.com>
2025-03-03 20:20:53 -0600

    unordered list implementation

--- UnOrderedLinkedList.py
+++ UnOrderedLinkedList.py
@@ -1,14 +1,14 @@
 class Node:
     
     def __init__(self,data):
-        self.data = data
+        self.item = data
         self.next = None
     
     def set_data(self, new_data):
-        self.data = new_data
+        self.item = new_data
         
     def get_data(self):
-        return self.data
+        return self.item
     
     def get_next(self):
         return self.next 
@@ -17,7 +17,7 @@ class Node:
         self.next = new_next
         
         
-class LinkedList:
+class UnorderedList:
     
     def __init__(self):
         self.head = None
@@ -27,7 +27,59 @@ class LinkedList:
         
     def add(self, item):
         temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
         
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+            
+        return count
+    
+    def search(self, item):
+        current = self.head
+        found = False
+        
+        while current and not found:
+            if(current.get_data() == item):
+                found = True
+            else:
+                current = current.get_next()
+        return found
+    
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        
+        while current and not found:
+            if(current.get_data() == item):
+                found = True
+                
+            else:
+                previous = current
+                current = current.get_next()
+        if previous == None:
+            self.head = current.get_next()
+            
+        else:
+            previous.set_next(current.get_next)
+            
+    def pop(self):
+        pass
+    
+    def append(self, item):
+        pass
+    
+    def insert(self, ind, item):
+        pass
+    
+    def index(self, item):
+        pass
+
         
     
     
