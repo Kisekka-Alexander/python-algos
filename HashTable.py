240a13aa7e91c3435b6d3a6285acf6c0f20b2c1c
Kisekka-Alexander <kkalexander717@gmail.com>
2025-03-07 23:26:29 -0600

    complete put method for hashtable

--- HashTable.py
+++ HashTable.py
@@ -16,11 +16,24 @@ class HashTable:
             if(self.slots[hash_value] == key):
                 self.data[hash_value] = val #Replace the value 
             
-            else:
+            else: #If the slot is already occupied, rehash to find another slot
+                
                 next_slot = self.rehash_function(hash_value, len(self.slots))
                 
+                # Loop through the slots until you find an empty slot self.slots[next_slot] == None OR
+                # Until you find a slot with the value == key 
                 while self.slots[next_slot] != None and self.slots[next_slot] != key:
                     next_slot = self.rehash_function(next_slot, len(self.slots))
+                
+                # If you find an empty slot then fill it   
+                if self.slots[next_slot] == None:
+                    self.slots[next_slot] = key
+                    self.data[next_slot] = val
+                    
+                # Otherwise if the next slot you find has value == key, then replace the value in the data table
+                else:
+                    self.data[next_slot] = val
+                    
     
     def hash_function(self,key,size):
         return key % size
